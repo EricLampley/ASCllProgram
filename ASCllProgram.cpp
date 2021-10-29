20958fc1ae9574ef1d43257bc588d55acd2ea721
EricLampley <EjLampley32@gmail.com>
2021-10-29 04:20:59 -0400

    Add project files.

---
+++ ASCllProgram.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std; 
+
+
+int main()
+{
+    char c1, c2, c3, c4, c5; 
+    cout << "Enter 5 letters: "; 
+    cin >> c1 >> c2 >> c3 >> c4 >> c5; 
+    cout << "ASCII message:  " << int(c1) << " "<<  int(c2) 
+    << " " << int(c3) << " " <<  int(c4) << " " <<  int(c5); 
+
+
+    system("pause>0"); 
+}
